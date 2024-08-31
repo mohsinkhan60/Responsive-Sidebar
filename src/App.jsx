4362def7002eea01d0854a60bcb3e617eb59c242
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-08-31 14:55:13 +0500

    add complete sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ export const App = () => {
   return (
     <main className="App">
       <Sidebar>
-        <SidebarItem icon={<LayoutDashboard size={20} />} text="DashBoard" />
+        <SidebarItem icon={<LayoutDashboard size={20} />} text="DashBoard" alert />
         <SidebarItem icon={<BarChart3 size={20} />} text="statistics" active />
         <SidebarItem icon={<UserCircle size={20} />} text="Users" />
         <SidebarItem icon={<Boxes size={20} />} text="Inventor" />
-        <SidebarItem icon={<Package size={20} />} text="Orders" />
+        <SidebarItem icon={<Package size={20} />} text="Orders" alert />
         <SidebarItem icon={<Receipt size={20} />} text="Billings" />
         <hr className="my-3" />
         <SidebarItem icon={<Settings size={20} />} text="Settings " />
